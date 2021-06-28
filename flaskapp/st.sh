c00dc14bc1e6df9ff8aa2dfb11c9b6e514c62a5f
jecadae <80022211+jecadae@users.noreply.github.com>
2021-06-28 18:32:15 +0700

    Create st.sh

---
+++ flaskapp/st.sh
@@ -0,0 +1,10 @@
+gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
+sleep 25
+echo start client
+python3 client.py
+APP_CODE=$?
+sleep 5
+echo $APP_PID
+kill -TERM $APP_PID
+echo app code $APP_CODE
+exit $APP_CODE
